6ce6f92cf94ac3abf22af14c513d39de94fe4018
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-07-19 22:44:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Smaller elements/count-smaller-elements.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+//  User function template for C++
+
+class Solution {
+  public:
+        int binarySearch(vector<int> &temp,int key){
+        int index=-1,low=0,high=temp.size()-1,mid;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(temp[mid]==key){
+                index=mid;
+                high=mid-1;
+            }
+            else if(temp[mid]>key)high=mid-1;
+            else low=mid+1;
+        }
+        return index;
+    }
+    vector<int> constructLowerArray(vector<int> &arr) {
+        // code here
+        vector<int> ans;
+        vector<int> temp;
+        for(auto x:arr)temp.push_back(x);
+        sort(temp.begin(),temp.end());
+        for(int i=0;i<arr.size();i++){
+            int index = binarySearch(temp,arr[i]);
+            ans.push_back(index);
+            temp.erase(temp.begin()+index);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        vector<int> a = ob.constructLowerArray(arr);
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
