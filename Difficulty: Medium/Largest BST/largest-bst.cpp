cb165c082b24a46c5b3dbe4ab55fa594f0703358
Paritosh Varshney <56546034+paritoshv@users.noreply.github.com>
2024-07-22 14:15:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -0,0 +1,164 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* Tree node structure  used in the program
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+
+vector<int> solve(Node *root){
+    //size, t/f, min, max
+    
+    if(root==NULL){
+        return {0,1,INT_MAX,INT_MIN};
+    }
+    
+    if(root->left==NULL && root->right==NULL){
+        return {1,1,root->data, root->data};
+    }
+    
+    vector<int> left = solve(root->left);
+    vector<int> right = solve(root->right);
+    
+    if(left[3] < root->data && right[2] > root->data && left[1]==1 && right[1]==1){
+        return {
+            left[0]+right[0]+1, 
+            left[3] < root->data && right[2] > root->data && left[1]==1 && right[1]==1,
+            min(left[2],root->data),
+            max(right[3],root->data)
+        };
+    } else {
+        return {max(left[0], right[0]),0,0,0};
+    }
+}
+
+class Solution{
+    public:
+    /*You are required to complete this method */
+    // Return the size of the largest sub-tree which is also a BST
+    int largestBst(Node *root)
+    {
+    	//Your code here
+        vector<int> ans =	solve(root);
+    	
+    	return ans[0];
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.largestBst(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
